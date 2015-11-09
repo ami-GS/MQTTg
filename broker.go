2576a2c5571090b66ef7b1694a2ce39557225172
ami-GS <1991.daiki@gmail.com>
2015-11-09 20:26:41 +0900

    change broker members

--- broker.go
+++ broker.go
@@ -1,7 +1,8 @@
 package MQTTg
 
 type Broker struct {
-	Bt        *Transport
-	ClientIDs []string
-	Users     map[string]*User // map[ClientIDs]*User
+	Bt      *Transport
+	Clients map[string]*Client // map[ClientIDs]*Client
+	//Topics map
+}
 }
